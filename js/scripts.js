c36eb0a57da62e73b9f31e65b8d398c2ed7d669e
алик Хабиров <khabirov_alekc@mail.ru>
2022-12-28 18:25:25 +0300

    код со второго дня

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+confirm("hello")
+function myFirstFunc(arg1,arg2){
+    return arg1+arg2
+}
+console.log(myFirstFunc(49,70))
+
+// console.log('Hello');
+// function Letter(str,num){
+// let newStr='';
+// let arr=[];
+// for(j=0;j<str.length;j++){
+// for(let i=0;i<num;i++){    newStr+=str[Math.floor(Math.random()*str.length)]
+// }return newStr}return arr.push(newStr[j] +newStr[j+1])
+// }
+// console.log(Letter('abcdef',3))
+
+// const twoSumm2=(arrMy)=>{
+// arrMy.sort((a,b)=>a-b)
+//     console.log('длина',arrMy.length,arrMy)
+// return arrMy[arrMy.length-1]+arrMy[arrMy.length-2]
+// }
+// console.log(twoSumm2([19,5,42,2,77]))
